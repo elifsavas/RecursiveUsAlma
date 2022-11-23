8383df88779f0255e18edccb36de4d3bd01b7ff2
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-11-23 16:52:42 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static int power(int m, int n) {
+    if (n>0)
+        return m * power(m,n-1) ;
+    else return 1 ;
+
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Taban Değeri Giriniz: ");
+        int m = input.nextInt();
+
+        System.out.print("Üs Değeri Giriniz: ");
+        int n = input.nextInt();
+
+        System.out.print("Sonuç: " + power(m, n));
     }
 }
